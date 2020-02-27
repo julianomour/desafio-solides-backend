fc8202eb3a85e1413c4ef115a31b66c5e79778ab
Juliano Moura <julianomoura05@gmail.com>
2020-02-27 15:47:38 -0300

    database credentials

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,19 @@
 module.exports = {
+  // dialect: 'postgres',
+  // host: 'localhost',
+  // username: 'postgres',
+  // password: 'solides',
+  // database: 'solidesDB',
+  // define: {
+  //   timestamps: true,
+  //   underscored: true,
+  //   underscoredAll: true,
+  // },
   dialect: 'postgres',
-  host: 'localhost',
-  username: 'postgres',
-  password: 'solides',
-  database: 'solidesDB',
+  host: 'ec2-52-203-160-194.compute-1.amazonaws.com',
+  username: 'xkchfxywshtlbz',
+  password: '3855b775b52b99c74e9a1b1ba7b5cfaf7e35764eff902b4101f4dc26739ea6d5',
+  database: 'dev61e95334qcc',
   define: {
     timestamps: true,
     underscored: true,
